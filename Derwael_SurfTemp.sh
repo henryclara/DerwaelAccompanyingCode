1f60775ef3c717b991c1695e50c9dffafbe09d00
Clara Henry <clara.henry@Claras-MacBook-Pro.local>
2022-05-10 15:26:18 +0200

    Convert Celsius to Kelvin

--- Derwael_SurfTemp.sh
+++ Derwael_SurfTemp.sh
@@ -2,4 +2,6 @@
 
 gmt grdconvert SurfTempInit.nc -R920750/997590/1880000/1965720 -GDerval_SurfTemp.nc -V
 
-cdo chname,z,tsurf Derval_SurfTemp.nc Derwael_SurfTemp.nc
+cdo addc,273.15 Derval_SurfTemp.nc Derval_SurfTemp2.nc
+
+cdo chname,z,tsurf Derval_SurfTemp2.nc Derwael_SurfTemp.nc
