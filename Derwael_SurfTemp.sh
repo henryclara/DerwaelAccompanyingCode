a5ad76a614ade83799e16991add7812ffdd92ad8
Clara Henry <clara.henry@Claras-MacBook-Pro.local>
2022-05-10 18:39:09 +0200

    Expand domain southward (issues reading in netcdf file in Elmer)

--- Derwael_SurfTemp.sh
+++ Derwael_SurfTemp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gmt grdconvert SurfTempInit.nc -R900750/1007590/1080000/2065720 -GDerval_SurfTemp.nc -V
+gmt grdconvert SurfTempInit.nc -R920750/997590/1800000/1965720 -GDerval_SurfTemp.nc -V
 
 cdo addc,273.15 Derval_SurfTemp.nc Derval_SurfTemp2.nc
 #cdo addc,273.15 SurfTempInit.nc Derval_SurfTemp2.nc
