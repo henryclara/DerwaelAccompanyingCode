cef0ebf249a8c2c3c4f02d32b5cfc34bce069d67
Clara Henry <clara.henry@mpimet.mpg.de>
2022-08-16 16:57:19 +0200

    Expand domain slightly

--- Derwael_SMB.sh
+++ Derwael_SMB.sh
@@ -2,8 +2,9 @@
 
 # SMB
 
-gmt grdconvert  RBIS_SMB_5km_in_m.nc                              -R920750/997590/1880000/1965720 -GSMB_Derval_5_5_km.nc        -V
+gmt grdconvert  RBIS_SMB_5km_in_m.nc                              -R919750/998590/1879000/1966720 -GSMB_Derwael_5_5_km.nc        -V
+# gmt grdconvert  RBIS_SMB_5km_in_m.nc                              -R920750/997590/1880000/1965720 -GSMB_Derval_5_5_km.nc        -V
 
-cdo chname,z,smb SMB_Derval_5_5_km.nc                                Derwael_SMB.nc
+cdo chname,z,smb SMB_Derwael_5_5_km.nc                                Derwael_SMB.nc
 
 
