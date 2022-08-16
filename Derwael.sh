8db33ca1f64b2b0d6fc34e03f262535372a5a948
Clara Henry <clara.henry@mpimet.mpg.de>
2022-08-16 16:48:54 +0200

    Adjust bedrock to get rid of anomaly

--- Derwael.sh
+++ Derwael.sh
@@ -12,11 +12,11 @@ gmt grdconvert "BedMachineAntarctica_2020-07-15_v02.nc?mask"      -R920750/99759
 gmt grdconvert "antarctic_ice_vel_phase_map_v01.nc?VX"            -R920750/997590/1880000/1965720 -GDerwael_vx.nc -V
 gmt grdconvert "antarctic_ice_vel_phase_map_v01.nc?VY"            -R920750/997590/1880000/1965720 -GDerwael_vy.nc -V
 
-cdo sub Derwael_surface.nc Derwael_thickness.nc                      Derwael_base_s.nc
-cdo chname,surface,zb Derwael_base_s.nc                              Derwael_base.nc
-cdo chname,surface,zs Derwael_surface.nc                             Derwael_surface2.nc
-cdo chname,bed,bedrock Derwael_bed.nc 		                     Derwael_bed2.nc
-cdo chname,VX,vx Derwael_vx.nc                                       Derwael_vx2.nc
-cdo chname,VY,vy Derwael_vy.nc                                       Derwael_vy2.nc
+cdo sub Derwael_surface.nc Derwael_thickness.nc                        Derwael_base_s.nc
+cdo chname,surface,zb Derwael_base_s.nc                                Derwael_base.nc
+cdo chname,surface,zs Derwael_surface.nc                               Derwael_surface2.nc
+cdo chname,VX,vx Derwael_vx.nc                                         Derwael_vx2.nc
+cdo chname,VY,vy Derwael_vy.nc                                         Derwael_vy2.nc
+cdo chname,__xarray_dataarray_variable__,bedrock DerwaelNewBedrock.nc  DerwaelNewBedrock2.nc
 
-cdo merge Derwael_surface2.nc Derwael_thickness.nc Derwael_base.nc Derwael_bed2.nc Derwael_mask.nc Derwael_vx2.nc Derwael_vy2.nc Derwael.nc
+cdo merge DerwaelNewBedrock2.nc Derwael_surface2.nc Derwael_thickness.nc Derwael_base.nc Derwael_bed.nc Derwael_mask.nc Derwael_vx2.nc Derwael_vy2.nc Derwael.nc
